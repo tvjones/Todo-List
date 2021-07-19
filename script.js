dd235287ecb9df988d294c0c3944a9fd05dd32c1
tvjones <43976584+tvjones@users.noreply.github.com>
2021-07-18 21:33:11 -0500

    Added option to reset list

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function add(){
+function add(tasks){
     //grab new task
     var task = document.getElementById("new-task");
     
     //create paragraph element and text node
     const text = document.createElement("p");
+    text.className = "list-item"
     const node = document.createTextNode(task.value);
 
     text.appendChild(node);
@@ -16,5 +17,15 @@ function add(){
         text.style.textDecoration = "line-through"
         text.style.color="red";
       } );
-
 }
+
+function clearList(){
+    var tasks = document.getElementsByClassName("list-item");
+    console.log(tasks.length);
+
+    //removing tasks
+    while(tasks.length>0){
+        tasks[0].remove();
+    }
+}
+
